c01cc7b1f670c65c52e6f79adb4da62e95747a75
almaz <almazgimaev@gmail.com>
2024-01-11 16:40:11 +0100

    Update fixed_columns in tables

--- src/ui/object_stats.py
+++ src/ui/object_stats.py
@@ -1,6 +1,6 @@
 from supervisely.app.widgets import Card, Container, FastTable
 
-fast_table = FastTable(fixed_columns=2)
+fast_table = FastTable(fixed_columns=1)
 
 card = Card(
     title="Objects",
