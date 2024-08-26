0f178e470e9233f6b5ccc8b79d13220d8f12e1d0
vorozhkog <vorozkhog@icloud.com>
2024-08-26 19:57:05 +0100

    add exception handling for deserialization

--- src/functions.py
+++ src/functions.py
@@ -95,7 +95,13 @@ def process_project():
                 video_frames = ds_frames.setdefault(video_info.name, {})
 
                 ann_info = g.api.video.annotation.download(video_info.id)
-                ann = sly.VideoAnnotation.from_json(ann_info, g.PROJECT_META, key_id_map)
+                try:
+                    ann = sly.VideoAnnotation.from_json(ann_info, g.PROJECT_META, key_id_map)
+                except Exception as e:
+                    err_msg = "An error occured while deserialization. Skipping annotation..."
+                    debug_info = {"json annotation": ann_info, "exception message": str(e)}
+                    sly.logger.error(err_msg, extra=debug_info)
+                    continue
                 process_video_annotation(ann, ds_objects, ds_figures, video_frames, obj_figures)
                 objkey2frames_cnt, objkey2tags = get_frames_tags_by_objects_on_videos(video_frames)
                 objkey2classname = {str(obj.key()): obj.obj_class.name for obj in ann.objects}
